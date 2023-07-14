4cce2425f1f2408039c4ba10c8c96d0d056b7ced
HesterG <hestergong@gmail.com>
2023-07-14 10:14:19 +0000

    fix checkbox production (#274)
    Reviewed-on: https://gitea.com/gitea/blog/pulls/274
    Co-authored-by: HesterG <hestergong@gmail.com>
    Co-committed-by: HesterG <hestergong@gmail.com>

--- src/components/Subscribe/index.js
+++ src/components/Subscribe/index.js
@@ -13,7 +13,7 @@ const Subscribe = ({placeholder, submitButtonText, className, classNameInputs}) 
     <form method="post" action="https://list.gitea.com/subscription/form" className={clsx(style.root, className)} onSubmit={() => {setLoading(true)}}>
       <div className={clsx(style.inputs, classNameInputs)}>
         <Input type="hidden" name="nonce" />
-        <Input className={style.checkbox} id="2aae7" type="checkbox" name="l" value="2aae7a49-b6b9-4aa3-b35a-32c9aeace57b" checked readOnly />
+        <input className={style.checkbox} id="2aae7" type="checkbox" name="l" value="2aae7a49-b6b9-4aa3-b35a-32c9aeace57b" checked readOnly />
         <Input
           className={style.input}
           name="email"
